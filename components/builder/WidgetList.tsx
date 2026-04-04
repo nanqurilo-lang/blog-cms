ef55746e3821d8e1268f6e127740671b5db90796
nanqurilo-lang <nanqurilo@gmail.com>
2026-04-04 15:42:17 +0530

    button class fixes

--- components/builder/WidgetList.tsx
+++ components/builder/WidgetList.tsx
@@ -2,7 +2,7 @@
 
 import { useEditor } from "./EditorProvider";
 
-const buttonClass = "btn inline-flex items-center gap-2 px-4 py-2 border border-gray-300 text-gray-700 text-sm font-medium rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-400 transition";
+const buttonClass = "btn inline-flex items-center gap-2 px-2 py-2 border border-gray-300 text-gray-700 text-sm font-medium rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-400 transition";
 
 export default function WidgetList({ addWidget }: any) {
   const { state } = useEditor();
@@ -10,62 +10,62 @@ export default function WidgetList({ addWidget }: any) {
 
   return (
     <div
-      className={`w-64 border-r p-4 space-y-2 ${
+      className={`w-64 border-r p-4 space-y-2 grid grid-cols-2 gap-1 ${
         isPreview ? "opacity-50 pointer-events-none" : ""
       }`}
     >
       <h3 className="mb-2 font-bold">Widgets</h3>
 
       <button onClick={() => addWidget("button")} className={buttonClass}>
-        Button
+        ➕ Button
       </button>
 
       <button onClick={() => addWidget("text")} className={buttonClass}>
-        Text Editor
+       📝 Text Editor
       </button>
 
       <button onClick={() => addWidget("heading")} className={buttonClass}>
-        Heading
+       🔤 Heading
       </button>
 
       <button onClick={() => addWidget("image")} className={buttonClass}>
-        Image
+        🖼 Image
       </button>
 
       <button onClick={() => addWidget("image-carousel")} className={buttonClass}>
-        Image Carousel
+       🖼️  Image Carousel
       </button>
 
       <button onClick={() => addWidget("icon-box")} className={buttonClass}>
-        Icon Box
+        🧩 Icon Box
       </button>
 
       <button onClick={() => addWidget("accordion")} className={buttonClass}>
-        Accordion
+      📂  Accordion
       </button>
 
       <button onClick={() => addWidget("inner-section")} className={buttonClass}>
-        Inner Section
+         📐 Inner Section
       </button>
 
       <button onClick={() => addWidget("paragraph")} className={buttonClass}>
-        Paragraph
+        📄 Paragraph
       </button>
 
       <button onClick={() => addWidget("separator")} className={buttonClass}>
-        Separator
+        ➖ Separator
       </button>
 
       <button onClick={() => addWidget("hero")} className={buttonClass}>
-        Hero Section
+      🧲  Hero Section
       </button>
 
       <button onClick={() => addWidget("testimonial")} className={buttonClass}>
-        Testimonial
+        💬 Testimonial
       </button>
 
       <button onClick={() => addWidget("video")} className={buttonClass}>
-        Video
+       ▶️ Video
       </button>
     </div>
   );
